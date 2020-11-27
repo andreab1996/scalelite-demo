0a3ebf849bf0973ebabbf2f700aa0c7d39795a54
Andrea Baco <andreabaco07@gmail.com>
2020-11-27 20:57:19 +0100

    remove unused imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,15 @@
+import 'font-awesome/css/font-awesome.min.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import App from "./App";
-import Login from "./components/Login";
-import Server from "./components/Server";
 import "./index.css";
 import store from "./store";
-import 'font-awesome/css/font-awesome.min.css';
 
 const rootElement = document.getElementById('root')
 ReactDOM.render(
 	<Provider store={store}>
 		<App />
-		{/* <Table /> */}
-		{/* <Server /> */}
-		{/* <Login /> */}
 	</Provider>,
 	rootElement
 )
